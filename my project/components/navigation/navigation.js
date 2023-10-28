7218691d7ff17964798a0f35b82e79bb4f3b19b4
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2023-10-28 22:02:03 +0600

    u

---
+++ my project/components/navigation/navigation.js
@@ -0,0 +1,73 @@
+import useFirebaseAuth from '../../hooks/useFirebaseAuth'
+import { useRouter } from 'next/router';
+import React from 'react'
+import styles from './navigation.module.css'
+
+
+const Navigation = ({ setIsNavOpen }) => {
+    const router = useRouter();
+    const { logOut, user } = useFirebaseAuth();
+
+
+
+    return (
+        <div className={styles.page}>
+            <div className={styles.navCloseContainer}>
+                <div className={styles.navClose} onClick={() => { setIsNavOpen(false) }} >
+                    close
+                </div>
+            </div>
+
+            <div className={styles.navLinks}>
+
+                <div className={styles.navLink}
+                    style={{
+                        backgroundColor: 'white',
+                        color: 'black',
+                        marginBottom: '10px',
+                    }}
+                >
+                    <div className={styles.navLinkText}>{user?.email}</div>
+                </div>
+
+                <div className={styles.navLink}
+                    onClick={() => { router.push('/') }}
+                >
+                    <div className={styles.navLinkText}>Home</div>
+                </div>
+
+                <div className={styles.navLink}
+                    onClick={() => { router.push('/store') }}
+                >
+                    <div className={styles.navLinkText}>Organic Shop</div>
+                </div>
+
+                <div className={styles.navLink}
+                    onClick={() => { router.push('/cart') }}
+                >
+                    <div className={styles.navLinkText}>My Cart</div>
+                </div>
+
+                <div className={styles.navLink}
+                    onClick={() => { router.push('/saved-list') }}
+                >
+                    <div className={styles.navLinkText}>Saved List</div>
+                </div>
+
+                <div className={styles.navLink}
+                    onClick={() => { router.push('/orders') }}
+                >
+                    <div className={styles.navLinkText}>Shopping List</div>
+                </div>
+
+                <div className={styles.navLink}
+                    onClick={async () => { await logOut(); router.push('/'); }}
+                >
+                    <div className={styles.navLinkText}>Log out</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navigation
