474328ac5eaf70b8f7d8311bb6b27b9862cd9568
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2023-10-28 22:02:03 +0600

    u

---
+++ my project/pages/cart.js
@@ -0,0 +1,90 @@
+import AuthUI from '@/components/AuthUI/AuthUI';
+import Navigation from '@/components/navigation/navigation';
+import { useCart } from '@/hooks/useCart';
+import { decreaseQuantityOfProductInCart, getCartTotal, increaseQuantityOfProductInCart, removeProductFromCart } from '@/utils/cartManager';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+
+import styles from '../styles/Cart.module.css'
+
+
+
+const Cart = () => {
+
+
+    const { products, gotoCart } = useCart();
+    const router = useRouter();
+
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+
+
+    if (isNavOpen) {
+        return (
+            <Navigation
+                setIsNavOpen={setIsNavOpen}
+            />
+        )
+    }
+
+
+    return (
+        <div className={styles.page}>
+
+            <div className={styles.topBar}>
+                <div className={styles.hamburger} onClick={() => { setIsNavOpen(true) }} />
+                <div className={styles.pageTitle}> Cart </div>
+                <div className={styles.cart} onClick={gotoCart} />
+            </div>
+
+            <div className={styles.products}>
+                {
+                    products.length === 0 &&
+                    <div className={styles.emptyCart}>
+                        <div className={styles.emptyCartText}>Your cart is empty</div>
+                    </div>
+                }
+                {
+                    products.map((product) => (
+                        <div className={styles.product} key={product.id}>
+                            <div className={styles.removeProductFromCart} onClick={() => { removeProductFromCart(product); router.reload() }} />
+
+                            <div className={styles.productInformation}>
+                                <div className={styles.productImage}>
+                                    <img src={product.image} alt={product.name} />
+                                </div>
+                                <div className={styles.productName}>{`${product.name} (${product.category?.toLowerCase()})`}</div>
+                                <div className={styles.productPrice}>{product.price} TK</div>
+                                <div> x {product.quantity} kg</div>
+                            </div>
+                            <div className={styles.productActionButtons}>
+                                <div className={styles.removeProduct} onClick={() => { decreaseQuantityOfProductInCart(product); router.reload() }} />
+                                <div className={styles.productQuantity}>{product.quantity} kg</div>
+                                <div className={styles.addProduct} onClick={() => { increaseQuantityOfProductInCart(product); router.reload() }} />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+
+
+            {
+                products.length > 0 &&
+                <div className={styles.checkout}>
+                    <div className={styles.checkoutButtonTotal}>{`Total: ${getCartTotal()} TK`} </div>
+                    <div className={styles.checkoutButton} onClick={() => { router.push('/checkout') }}>Checkout  </div>
+                </div>
+            }
+
+        </div>
+    )
+}
+
+
+export default function Page() {
+    return (
+        <AuthUI InnerComponent={Cart} />
+    )
+}
+
+
