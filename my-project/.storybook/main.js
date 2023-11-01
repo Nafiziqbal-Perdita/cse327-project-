c8e06c56c275e7ef5d7f615420ffa8a8d4adc191
unknown <nafiziqbal.mo@gmail.com>
2023-11-01 16:13:07 +0600

    Updated by Nafiz

--- my-project/.storybook/main.js
+++ my-project/.storybook/main.js
@@ -9,8 +9,6 @@ const config = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-actions",
-    "@storybook/addon-docs"
   ],
   framework: {
     name: "@storybook/nextjs",
