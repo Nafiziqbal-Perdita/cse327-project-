7113ad6965bc66b016f929d101ad5361f592c8ce
Jeesan57 <115409557+Jeesan57@users.noreply.github.com>
2023-11-01 14:04:54 +0600

    update

---
+++ hooks/useProducts.js
@@ -0,0 +1,22 @@
+import products from "../data/products"
+import { useEffect, useState } from "react";
+import { upsertProduct, getAllProducts } from "../database/database_functions";
+
+
+// write a simple hook that returns a list of products
+export const useProducts = () => {
+    const [loadedProducts, setProducts] = useState([]);
+
+
+
+    useEffect(() => {
+        let fetchAllProducts = async () => {
+            const products = await getAllProducts();
+            setProducts(products);
+        }
+        fetchAllProducts();
+    }, []);
+    return { products: loadedProducts };
+}
+
+
