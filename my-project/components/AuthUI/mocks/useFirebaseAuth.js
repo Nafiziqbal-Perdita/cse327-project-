97409f7cda8ccec2da01a067510df47333693410
unknown <nafiziqbal.mo@gmail.com>
2023-11-06 05:09:13 +0600

    structure

--- my-project/components/AuthUI/mocks/useFirebaseAuth.js
+++
@@ -1,9 +0,0 @@
-export const useFirebaseAuth = () => ({
-    user: null,
-    forgetPassword: jest.fn(),
-    isLoading: false,
-    signUp: jest.fn(),
-    signIn: jest.fn(),
-    error: null,
-    setError: jest.fn(),
-  });
